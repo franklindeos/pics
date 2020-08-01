a1b7b934cfb2ebb670dc345895fffe993b51612e
Franklin Soares <franklindeos@gmail.com>
2020-08-01 13:27:44 -0300

    Paginação Simples

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,17 +1,24 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnInit, OnDestroy } from '@angular/core'
 import { ActivatedRoute } from '@angular/router'
+import { Subject } from 'rxjs/internal/Subject'
+import { debounceTime } from 'rxjs/operators'
 
 @Component({
   selector: 'app-photo-list',
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.sass']
 })
-export class PhotoListComponent implements OnInit {
+export class PhotoListComponent implements OnInit,OnDestroy {
 
   title = 'Alurapic'
   photos = []
   filter: string = ''
-  nome = 'flavio'
+  debounce: Subject<string> = new Subject<string>(); // Subject from RxJS
+  /**
+   * debounce vai escutar a mudança no campo de filtro e após um tempo configurado via pipe
+   * irá aplicar o filtro
+   */
+
   constructor(
     private activatedRoute: ActivatedRoute
   ) { }
@@ -22,6 +29,19 @@ export class PhotoListComponent implements OnInit {
      * e preenche em photos
      */
     this.photos = this.activatedRoute.snapshot.data.photos
+    /**
+     * Incrição no debounce para receber no filtro os valores digitados
+     * em photo-list.component.html
+     */
+    this.debounce.pipe(debounceTime(300)).subscribe(filter => this.filter = filter);
+  }
+
+  ngOnDestroy(): void {
+    /**
+     * Caso não seja feita a remoção da lista debounce irá continuar com o valor recebido
+     * mesmo após a destruição do componente gerando um memory leak.
+     */
+    this.debounce.unsubscribe();
   }
 
 }
--- src/app/photos/photo/photo.service.ts
+++ src/app/photos/photo/photo.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Photo } from "./photo";
 
@@ -11,5 +11,18 @@ export class PhotoService {
 
     listFromUser(userName: string) {
             return this.http.get<Photo[]>(API + '/' + userName + '/photos');
+    }
+
+    /**
+     * 
+     * @param userName 
+     * @param page pagina para navegação. Inicia em 1 em photo-list.resolver.ts
+     */
+    listFromUserPaginated(userName: string, page: number) {
+        const params = new HttpParams()
+        .append('page', page.toString());
+        return this.http
+        .get<Photo[]>(API + '/' + userName + '/photos', { params });
+        //Pede ao back-end a lista paginada http://localhost:3000/flavio/photos?page=2
     }
 }
