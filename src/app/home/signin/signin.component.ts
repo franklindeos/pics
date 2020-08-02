ad3c5da16e41b20cca8c01b42417dca8a972d79f
Franklin Soares <franklindeos@gmail.com>
2020-08-02 13:58:50 -0300

    Message template and autofocus

--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef} from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from '../../auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   templateUrl: './signin.component.html',
@@ -8,8 +10,12 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 export class SigninComponent implements OnInit{
 
   loginForm: FormGroup
+  @ViewChild('userNameInput') userNameInput: ElementRef<HTMLInputElement>;
 
-  constructor(private formBuilder: FormBuilder) { }
+
+  constructor(private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private router: Router) { }
 
   ngOnInit(): void {
     this.loginForm = this.formBuilder.group({
@@ -17,5 +23,21 @@ export class SigninComponent implements OnInit{
       password: ['', Validators.required]
     });
   }
+
+  login() {
+    const userName = this.loginForm.get('userName').value;
+    const password = this.loginForm.get('password').value;
+
+    this.authService
+        .authenticate(userName, password)
+        .subscribe(
+            () => this.router.navigate(['user', userName]),
+            err => {
+                console.log(err);
+                this.loginForm.reset();
+                this.userNameInput.nativeElement.focus();
+                alert('Invalid user name or password')
+            }
+        );  }
 
 }
